186cbff64b1b9c3cf6ffa81fb897647c58dd946e
Elias <elias.guo@nienmade.com.cn>
2024-12-12 17:51:56 +0800

    init

---
+++ src/pages/user-center/user-center.vue
@@ -0,0 +1,15 @@
+<template>
+	<view>
+		<up-text type="primary" text="我用十年青春,赴你最后之约"></up-text>
+	</view>
+</template>
+
+<script lang="ts" name="user-center" setup>
+	import { type Ref, ref } from "vue"
+
+	let username : Ref<string> = ref("123")
+</script>
+
+<style lang="scss" scoped>
+
+</style>
